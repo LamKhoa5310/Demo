ed66cf2a49288d41a23a5c5abf16a0647b304290
LamKhoa5310 <khoadohuu2004@gmail.com>
2024-08-20 23:02:37 +0700

    init project

---
+++ home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        img{
+            width: 500px;
+            height: 500px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+echo "Hello, this is LamKhoa script";
+echo '<h1 style="color: blue; text-align: center;">Xin chào TMU</h1>';
+echo'<img src="image.JPG" alt="z5678374355126_aef04d6064dd55a26111d721ef0745a0.jpg">'
+
+?>
+ 
+ <p>Xin chào</p>
+
+</body>
+</html>
